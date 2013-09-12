03bcacd20e88b5707fc7f281880a571aad8f9b3d
partus <gmenua@gmail.com>
2013-09-12 13:03:49 +0400

    ups

--- helpers.operators.js
+++ helpers.operators.js
@@ -29,7 +29,7 @@ if (typeof Handlebars !== 'undefined') {
     });
 
     Handlebars.registerHelper('$neq', function(v1, v2, options) {
-      if(v1 !=== v2) {
+      if(v1 !== v2) {
         return options.fn(this);
       }
        return options.inverse(this);
